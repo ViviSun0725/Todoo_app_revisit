275fe3456e1ee7e70b58c95bb8e647a9934302f5
Vivi <sun20317@gmail.com>
2025-04-08 15:46:49 +0800

    新增功能: 使用者註冊

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
+import axios from 'axios';
+
 const main = () => ({
     section: "signUp",
+    email: "",
+    nickname:"",
+    password:"",
     showLogin() {
         this.section = "login"
     },
@@ -8,6 +13,29 @@ const main = () => ({
     },
     showTask() {
         this.section = "task"
+    },
+    async signUp() {
+        if (this.email !== "" & this.nickname !== "" & this.password !== "") {
+            const url = "https://todoo.5xcamp.us/users";
+            const userData = {
+            user : {
+                email: this.email,
+                nickname: this.nickname,
+                password: this.password
+            }
+        }
+        try {
+            const resp = await axios.post(url, userData);
+            console.log(resp);
+            // 提示視窗 註冊成功
+        } catch(err) {
+            const errMessage = err.response.data.error.join(" / ");
+            console.log(errMessage);
+            // 提示視窗 註冊失敗
+        }
+        
+        }
+        
     }
 });
 
