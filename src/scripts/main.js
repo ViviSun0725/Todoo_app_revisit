d992fbccc989abb34fc64d75d23a1b80d42d2530
Vivi <sun20317@gmail.com>
2025-04-10 15:16:38 +0800

    新增功能：使用者登入 頁面初始化

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 import Toastify from 'toastify-js';
 import "toastify-js/src/toastify.css";
 
+const USER_TOKEN = "user-token";
+
 const main = () => ({
     section: "signUp",
     email: "",
     nickname:"",
     password:"",
+    isLogin: false,
     showLogin() {
         this.section = "login"
     },
@@ -21,8 +24,17 @@ const main = () => ({
         this.nickname = "";
         this.password = "";
     },
+    init() {
+        const token = localStorage.getItem(USER_TOKEN);
+        if(token) {
+            this.isLogin = true;
+            this.showTask();
+        } else {
+            this.showSignUp();
+        }
+    },
     async signUp() {
-        if (this.email !== "" & this.nickname !== "" & this.password !== "") {
+        if (this.email !== "" && this.nickname !== "" && this.password !== "") {
             const url = "https://todoo.5xcamp.us/users";
             const userData = {
             user : {
@@ -70,6 +82,47 @@ const main = () => ({
         
         }
         
+    },
+    async login(){
+        if (this.email !== "" && this.password !== "") {
+            const url = "https://todoo.5xcamp.us/users/sign_in";
+            const userData = {
+                user: {
+                    email: this.email,
+                    password: this.password
+                }
+            }
+            try {
+                const resp = await axios.post(url, userData);
+                const token = resp.headers.authorization;
+                console.log(token);
+                localStorage.setItem(USER_TOKEN, token);
+                this.isLogin = true;
+                Toastify({
+                    text: "登入成功",
+                    duration: 3000,
+                    gravity: "top",
+                    position: "center",
+                    stopOnFocus: true,
+                    style: {
+                        background: "linear-gradient(to right, #00b09b, #96c93d)",
+                    }
+                  }).showToast();
+            } catch(err) {
+                console.log(err);
+                Toastify({
+                    text: "登入失敗",
+                    duration: 3000,
+                    gravity: "top",
+                    position: "center",
+                    stopOnFocus: true,
+                    style: {
+                      background: "linear-gradient(to right, #FF6A88, #FF99AC)",
+                    }
+                  }).showToast();
+            }
+        }
+
     }
 });
 
