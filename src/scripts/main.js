c1502aff5ee6441ff2a7d192811df0d005a1f872
Vivi <sun20317@gmail.com>
2025-04-10 14:08:23 +0800

    刪除檔案：todoo-app-main資料夾

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,6 +14,11 @@ const main = () => ({
     showTask() {
         this.section = "task"
     },
+    clearInput() {
+        this.email = "";
+        this.nickname = "";
+        this.password = "";
+    },
     async signUp() {
         if (this.email !== "" & this.nickname !== "" & this.password !== "") {
             const url = "https://todoo.5xcamp.us/users";
@@ -25,13 +30,20 @@ const main = () => ({
             }
         }
         try {
+            console.log(userData);
             const resp = await axios.post(url, userData);
+            console.log("ok");
             console.log(resp);
             // 提示視窗 註冊成功
+
+            this.clearInput();
+            this.showLogin();
+
         } catch(err) {
-            const errMessage = err.response.data.error.join(" / ");
-            console.log(errMessage);
-            // 提示視窗 註冊失敗
+            if (err.response.data) {
+                const errMessage = err.response.data.error.join(" / ");
+                console.log(errMessage);
+            }
         }
         
         }
--- todoo-app-main/src/scripts/todo.js
+++
@@ -1,4 +0,0 @@
-import { initPages } from "./pages"
-initPages()
-
-// 程式碼寫在這裡
